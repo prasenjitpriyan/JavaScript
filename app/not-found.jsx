b318ca226c2e2eb8eb3082fd0dd53d2196c020fc
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-11-16 20:42:35 +0530

    not found page modified

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -8,9 +8,9 @@ const NotFoundPage = () => {
         <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
           <div className="mx-auto max-w-screen-sm text-center">
             <h1 className="text-minion-yellow mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl">
-              ~404~
+              404
             </h1>
-            <p className="mb-4 text-3xl font-bold tracking-tight text-black md:text-4xl">
+            <p className="mb-4 text-3xl font-bold tracking-tight text-gray-300 md:text-4xl">
               Something&#39;s missing.
             </p>
             <p className="mb-4 text-lg font-light text-gray-300">
@@ -19,7 +19,7 @@ const NotFoundPage = () => {
             </p>
             <Link
               href="/"
-              className="hover:bg-minion-yellow focus:ring-minion-yellow dark:focus:ring-primary-900 hover:text-dark-charcoal my-4 inline-flex items-center justify-center rounded-lg bg-black px-5 py-2.5 text-sm font-medium text-white focus:outline-none focus:ring-4"
+              className="hover:bg-minion-yellow hover:text-dark-charcoal focus:ring-minion-yellow my-4 inline-flex items-center justify-center rounded-lg bg-black px-5 py-2.5 text-sm font-medium text-white focus:ring-4"
             >
               Back to Homepage
             </Link>
